e3c272304aadfafa9f5935fed83e8eefd6e9a4c2
Bill Hause <bill.hause@yahoo.com>
2022-01-20 22:11:51 -0700

    Added some buttons to the interface.  Need to fix AttributeGraph warning when clicking the 'Orient Map' button

--- BushwhackGPS/View/ContentView.swift
+++ BushwhackGPS/View/ContentView.swift
@@ -16,11 +16,66 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 Text("Bongo")
+                MapView(theMap_ViewModel: theMap_ViewModel)
                 Text("Banana")
             } // VStack
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Picker("What kind of map do you want", selection: $theMap_ViewModel.isHybrid) {
+                        Text("Hybrid").tag(true)
+                        Text("Standard").tag(false)
+                    }
+                    .pickerStyle(.segmented)
+                    .onChange(of: theMap_ViewModel.isHybrid) { value in
+//                        print("Hybrid Picker Called \(value)")
+                    }
+                    
+                    Button(action: orientMap) {
+                        let theColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0)
+                        let imageString = theMap_ViewModel.getOrientMapImageName()
+                        Label("Center & Zoom", systemImage: imageString)
+                            .foregroundColor(Color(theColor))
+                    } //.font(.largeTitle) .padding()
+                        .labelStyle(HorizontalLabelStyle())
+                    
+                }
+            }
+            // Detect moving back to foreground
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                theMap_ViewModel.orientMap() // Re-orient map when app moves back to the foreground
+                AppSettingsEntity.getAppSettingsEntity().incrementUsageCount() // Count usage to know when to display the request for a review
+                MyLog.debug("Moved back to foreground wdh")
+            }
+        } // NavigationView
+    }
+
+    private func orientMap() {
+        withAnimation {
+            Haptic.shared.impact(style: .heavy)
+            theMap_ViewModel.orientMap() // Call intent function
+        }
+    }
+
+}
+
+// MARK: Custom Label Styles
+struct VerticalLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            configuration.icon.font(.headline)
+            configuration.title.font(.subheadline)
         }
     }
 }
+struct HorizontalLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title.font(.subheadline)
+            configuration.icon.font(.system(size: 24)) // .headline, .largeTitle, .subheadline,
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
