b1a689b7b567d749529dd8b38f976a0aa4f8e3d7
Bill Hause <bill.hause@yahoo.com>
2022-03-01 11:44:16 -0700

    Working on Marker Journal Entry View

--- BushwhackGPS/View/NewMarkerView.swift
+++ BushwhackGPS/View/NewMarkerView.swift
@@ -41,17 +41,51 @@ struct NewMarkerView: View {
 // already checked in the parrent view
 struct MarkerEditView: View {
     @ObservedObject var theMap_ViewModel: Map_ViewModel
-        
+    
+    @State var titleText: String = ""
+    @State var bodyText: String = ""
+    @State var iconName: String = "triangle"
+    @State var lat: Double = 100.0
+    @State var lon: Double = 100.0
+    @State var colorRed: Double = 0.0
+    @State var colorGreen: Double = 0.0
+    @State var colorBlue: Double = 0.0
+    @State var colorAlpha: Double = 1.0 // no transparency
+    
+    // Constants
+    let LEFT_PADDING = 10.0 // Padding on the left side of various controls
+    
     init(theMap_VM: Map_ViewModel) {
         theMap_ViewModel = theMap_VM
     }
+    
     var body: some View {
         VStack {
-            Text("Contineu Here")
+            TextDataInput(title: "Title", userInput: $titleText)
+//            HStack {
+//                Text("Journal Entry")
+//                Spacer()
+//            }
+            TextDataInputMultiLine(title: "Journal Entry", userInput: $bodyText)
+//            TextEditor(text: $bodyText)
+//                .multilineTextAlignment(.leading)
+//                .border(Color.black)
+            HStack {
+                Text("Latitude: \(lat)")
+                Spacer()
+            }
+            HStack {
+                Text("Longitude: \(lon)")
+                Spacer()
+            }
         }
+        .padding()
+//      .padding(EdgeInsets(top: 0.0, leading: LEFT_PADDING, bottom: 0, trailing: 10))
+        .navigationTitle("New Journal Marker") // Title at top of page
         .onAppear { HandleOnAppear() }
         .onDisappear { HandleOnDisappear() }
     }
+
     
     // This will be called when ever the view apears
     // Calling this from .onAppear in the Body of the view.
@@ -71,3 +105,38 @@ struct MarkerEditView_Previews: PreviewProvider {
         MarkerEditView(theMap_VM: Map_ViewModel()).environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
+
+
+// Generic data input with a label and field to enter data
+struct TextDataInput: View {
+    var title: String
+    @Binding var userInput: String
+    
+    var body: some View {
+        HStack(alignment: VerticalAlignment.center) {
+            Text(title)
+                .font(.body)
+            TextField("Enter \(title)", text: $userInput)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+//        .padding()
+    }
+}
+
+// Generic data input with a label and field to enter data
+struct TextDataInputMultiLine: View {
+    var title: String
+    @Binding var userInput: String
+    
+    var body: some View {
+        HStack(alignment: VerticalAlignment.center) {
+            Text(title)
+                .font(.body)
+            TextEditor(text: $userInput)
+                .multilineTextAlignment(.leading)
+                .border(Color.black)
+        }
+//        .padding()
+    }
+}
+
